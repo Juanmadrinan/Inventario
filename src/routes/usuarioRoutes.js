c8185f0921daa66c26a1fce1b3f647d4e877b550
Juan Madrinan <125837348+Juanmadrinan@users.noreply.github.com>
2024-06-12 17:01:09 -0500

    Update

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,8 +4,11 @@ const usuarioController = require('../controllers/usuarioController');
 
 router.get('/', usuarioController.getAllUsuarios);
 router.get('/:id', usuarioController.getUsuarioById);
+
 router.post('/', usuarioController.createUsuario);
+router.post('/register', usuarioController.registerUsuario);
 router.post('/login', usuarioController.loginUsuario);
+
 router.put('/:id', usuarioController.updateUsuario);
 router.delete('/:id', usuarioController.deleteUsuario);
 
